de6cc20b67fcc53381ba6628060afb0977de406d
kyjoon0916 <59378116+kyjoon0916@users.noreply.github.com>
2021-12-27 15:26:31 +0900

    test

--- mysite/UI/urls.py
+++ mysite/UI/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('company/', views.company, name='company'),
+    path('test/', views.test, name='test'),
+    path('model/', views.model, name='model'),
+    path('label/', views.label, name='label'),
+    path('model2/', views.model2, name='model2'),
+
 ]
